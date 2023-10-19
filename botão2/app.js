bfe92ac26e3f1c5b2447000a7fcfbdbccde029ae
Sabrina Araújo <82546575+Sabrinaara@users.noreply.github.com>
2023-10-19 11:16:14 -0300

    Add files via upload

---
+++ botão2/app.js
@@ -0,0 +1,19 @@
+const btnDark = document.querySelector('.btn');
+const container = document.querySelector('.container');
+
+
+btnDark.onclick = function() {
+    this.classList.toggle('active')
+    container.classList.toggle('active')
+}
+
+
+
+
+
+
+
+
+
+
+
